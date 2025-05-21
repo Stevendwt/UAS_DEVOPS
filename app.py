da9dfb83493e12a54daed0915efc54cfeae4e3eb
Steven Dwi Tirta Nugraha <118582914+Stevendwt@users.noreply.github.com>
2025-05-21 17:19:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template_string, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello from Flask!'
+# Template login
+login_template = '''
+<!doctype html>
+<title>Login</title>
+<h2>Login Page</h2>
+<form method="post">
+  Username: <input type="text" name="username"><br><br>
+  Password: <input type="password" name="password"><br><br>
+  <input type="submit" value="Login">
+</form>
+'''
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            return '<h1>Login successful!</h1>'
+        else:
+            return '<h1>Login failed. Try again.</h1>'
+    return render_template_string(login_template)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
